f8ed13c57df4fcd49ec6166cec46bbc69705a09d
Mally13 <mallymutuku13@gmail.com>
2020-02-13 13:37:14 +0300

    new

--- Calculations/src/com/example/calculations/RunningTotals.java
+++ Calculations/src/com/example/calculations/RunningTotals.java
@@ -1,4 +1,29 @@
 package com.example.calculations;
 
-public class RunningTotals {
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class RunningTotals
+{
+    public static void main(String[] args)
+    {
+        int days;
+        double sales;
+        double totalSales=0.0; //running total
+        DecimalFormat dollar=new DecimalFormat("#,##0.00");
+
+        String input= JOptionPane.showInputDialog("For how many days do you have sales "+
+                "figures?");
+        days= Integer.parseInt(input);
+
+        for (int count=1; count <=days; count++)
+        {
+            input=JOptionPane.showInputDialog("Enter the sales for day" +count);
+            sales= Double.parseDouble(input);
+
+            totalSales= totalSales += sales;
+        }
+        JOptionPane.showMessageDialog(null,"The total sales are: $" + dollar.format(totalSales));
+        System.exit(0);
+    }
 }
