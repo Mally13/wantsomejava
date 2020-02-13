12923feee495208d650942d407c12ec6206777bf
Mally13 <mallymutuku13@gmail.com>
2020-02-13 12:32:24 +0300

    new

--- Calculations/src/com/example/calculations/UserControlledLoop.java
+++ Calculations/src/com/example/calculations/UserControlledLoop.java
@@ -1,4 +1,15 @@
 package com.example.calculations;
 
-public class UserControlledLoop {
+import javax.swing.*;
+
+public class UserControlledLoop
+{
+    public static void main(String[] args)
+    {
+     int maxValue;
+     String input = JOptionPane.showInputDialog("How high should I go to square the nr?");
+     maxValue=Integer.parseInt(input);
+
+
+    }
 }
