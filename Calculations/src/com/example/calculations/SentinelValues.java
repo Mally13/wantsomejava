f796d703f85425fc630e74c1d3fd19a4b487519c
Mally13 <mallymutuku13@gmail.com>
2020-02-15 11:13:01 +0300

    new

--- Calculations/src/com/example/calculations/SentinelValues.java
+++ Calculations/src/com/example/calculations/SentinelValues.java
@@ -1,4 +1,4 @@
 package com.example.calculations;
 
-public class SentineValues {
+public class SentinelValues {
 }
---
+++ Files/src/com/example/textfiles/WritingToFiles.java
@@ -0,0 +1,4 @@
+package com.example.textfiles;
+
+public class WritingToFiles {
+}
