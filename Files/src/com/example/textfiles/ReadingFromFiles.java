4e646e83396c04c53d1560f49892698755c88a95
Mally13 <mallymutuku13@gmail.com>
2020-02-15 11:13:20 +0300

    new

--- Files/src/com/example/textfiles/ReadingFromFiles.java
+++ Files/src/com/example/textfiles/ReadingFromFiles.java
@@ -1,4 +1,48 @@
 package com.example.textfiles;
 
-public class ReadingFromFiles {
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadingFromFiles
+{
+    public static void main(String[] args) throws IOException
+    {
+        int sum=0;
+        int count =0;
+        double average;
+
+
+        File file = new File("OutputFile.txt");
+        if (file.exists())
+        {
+            Scanner inputFile =new Scanner(file);
+/*      THE NEXT METHOD without a while loop(CAN BE PROBLEMATIC IF AN OBJECT DOES NOT EXIST)
+              System.out.println(inputFile.nextLine());
+              System.out.println(inputFile.nextLine());
+              System.out.println(inputFile.nextLine());
+*/
+
+
+//THE WHILE LOOP + hasNext()
+            while (inputFile.hasNext())
+            {
+               sum= sum + inputFile.nextInt();
+               count++;
+            }
+            inputFile.close();
+            average= sum/ (double)count;
+            JOptionPane.showMessageDialog(null,"Sum of all values: " +sum + "\n" +
+                    "The number of values: " + count + "\n" +
+                    "The average is: " + average
+            );
+
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null," The file does" +
+                    "not exist!");
+        }
+    }
 }
--- Files/src/com/example/textfiles/WritingToFiles.java
+++ Files/src/com/example/textfiles/WritingToFiles.java
@@ -1,4 +1,21 @@
 package com.example.textfiles;
 
-public class WritingToFiles {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WritingToFiles
+{
+    public static void main(String[] args) throws IOException
+    {
+        //creates and opens file
+        // if file exists it replaces file
+        PrintWriter outputFile= new PrintWriter("OutputFile.txt");
+
+        outputFile.println("This is line 1");
+        outputFile.println("This is line 2");
+        outputFile.println("This is line 3");
+
+        outputFile.close();
+
+    }
 }
